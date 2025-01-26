1975482b7a6766f11b7ababea7d8678411a39ce7
Davi gomes <davigomesdev@gmail.com>
2025-01-25 21:10:07 -0300

    adicionando comentários ao arquivo unless.rb

--- unless.rb
+++ unless.rb
@@ -4,6 +4,8 @@
 puts "Qual é sua idade ?"
 idade = gets.chomp.to_i
 
+# o Unless serve basicamente como uma condição de excessão é quase a mesma coisa que
+# o if else porém relacionado prioritariamente a excessão
 unless idade <= 18
   puts "Você É Maior De Idade!"
 else
