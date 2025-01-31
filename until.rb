486dd77b28560fd794e061bbf9347a2536f4ae8b
Davi gomes <davigomesdev@gmail.com>
2025-01-31 15:27:01 -0300

    modificando console.txt e alterações

--- until.rb
+++ until.rb
@@ -1,6 +1,7 @@
 # o until serve basicamente igual o while porém ele executa um bloco de código enquanto a condição for falsa
 
 #utilizasse aqui o mesmo código do while
+
 contador = 0
 
 until contador > 100
